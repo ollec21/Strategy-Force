cbfc2ad0d82b33e22496858f486b3caffd77d733
kenorb <kenorb@users.noreply.github.com>
2021-01-11 07:42:22 +0000

    Optimize SignalOpen params (pf=1.51; dd=4.73; tt=556)

--- config/EURUSD_M1.h
+++ config/EURUSD_M1.h
@@ -19,13 +19,13 @@ struct Stg_Force_Params_M1 : StgParams {
   Stg_Force_Params_M1() : StgParams(stg_force_defaults) {
     lot_size = 0;
     signal_open_method = 0;
-    signal_open_filter = 1;
-    signal_open_level = (float)-10.0;
-    signal_open_boost = 0;
+    signal_open_filter = 0;
+    signal_open_level = (float)-30.0;
+    signal_open_boost = 1;
     signal_close_method = 0;
     signal_close_level = (float)45.0;
     price_stop_method = 0;
-    price_stop_level = (float)25.0;
+    price_stop_level = (float)45.0;
     tick_filter_method = 1;
     max_spread = 0;
   }
--- config/EURUSD_M30.h
+++ config/EURUSD_M30.h
@@ -19,7 +19,7 @@ struct Stg_Force_Params_M30 : StgParams {
   Stg_Force_Params_M30() : StgParams(stg_force_defaults) {
     lot_size = 0;
     signal_open_method = 0;
-    signal_open_filter = 1;
+    signal_open_filter = 0;
     signal_open_level = (float)10.0;
     signal_open_boost = 1;
     signal_close_method = -4;
