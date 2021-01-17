b6f8bed8f51380938a33a7102fca491688081424
kenorb <kenorb@users.noreply.github.com>
2021-01-17 00:30:17 +0000

    config/sets: Updates params

--- config/EURUSD_M1.h
+++ config/EURUSD_M1.h
@@ -6,8 +6,8 @@
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_Force_Params_M1 : ForceParams {
   Indi_Force_Params_M1() : ForceParams(indi_force_defaults, PERIOD_M1) {
-    applied_price = (ENUM_APPLIED_PRICE)3;
-    ma_method = (ENUM_MA_METHOD)0;
+    applied_price = (ENUM_APPLIED_PRICE)2;
+    ma_method = (ENUM_MA_METHOD)3;
     period = 4;
     shift = 0;
   }
